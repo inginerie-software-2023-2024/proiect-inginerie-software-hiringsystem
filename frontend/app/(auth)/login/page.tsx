58631e8f60acc59bcbaaeeeee75ca4e779839ac4
DragosGhinea <dragos.dev12@gmail.com>
2023-12-02 19:43:39 +0200

    Login page quick test code removal

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,12 +1,7 @@
 import AuthenticationTabs from "@/components/auth/AuthenticationTabs";
-import { getServerSession } from "@/lib/sessionServerActions";
 import React from "react";
 
 const Login = async () => {
-  const session = await getServerSession();
-
-  if (session.isLoggedIn) return <div>Already logged in: {session.email}</div>;
-
   return <AuthenticationTabs defaultTab="login" />;
 };
 
