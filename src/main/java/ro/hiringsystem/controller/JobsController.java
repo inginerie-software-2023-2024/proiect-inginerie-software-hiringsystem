7cee6992e15cfb95dc22214d3b402310f1929ad6
stefaniarincu <stefania.rincu@gmail.com>
2024-01-03 14:57:19 +0200

    add permissions for Job and JobApplications Controllers

--- src/main/java/ro/hiringsystem/controller/JobsController.java
+++ src/main/java/ro/hiringsystem/controller/JobsController.java
@@ -2,15 +2,13 @@ package ro.hiringsystem.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ro.hiringsystem.model.dto.JobDto;
-import ro.hiringsystem.model.entity.Job;
 import ro.hiringsystem.model.enums.JobType;
 import ro.hiringsystem.model.enums.Position;
 import ro.hiringsystem.service.JobService;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,29 +19,8 @@ public class JobsController {
 
     private final JobService jobService;
 
-    @GetMapping("/display")
-    String getUser(Model model) {
-
-        model.addAttribute("job", Job.builder()
-                .id(UUID.randomUUID())
-                .title("Software developer")
-                .description("Our team is high-talent and high-energy, creating new technologies that must achieve the best protection in the marketplace. Our Trainees will develop independent modules or products, working closely with our Senior Developers. The aim is to develop practical designs and then implement them, going through all the stages of a research project, including a public release")
-                .jobType(JobType.INTERNSHIP_ONE_MONTH)
-                .position(Position.INTERN)
-                .salary(4200.0)
-                .hoursPerWeek(8)
-                .startDate(LocalDate.of(2023, 7, 1))
-                .skillsNeeded(List.of("Basic OOP knowledge", "Team management"))
-                .offers(List.of("Great trainees", "New technologies"))
-                .build());
-
-        return "jobDisplay";
-
-    }
-
     @GetMapping("/get")
-    public ResponseEntity<JobDto> get (@RequestParam(required = true) UUID id) {
-        System.out.println("TEST GET"+id);
+    public ResponseEntity<JobDto> get(@RequestParam(required = true) UUID id) {
         return ResponseEntity.ok(jobService.getById(id));
     }
 
@@ -56,16 +33,19 @@ public class JobsController {
     public ResponseEntity<List<JobDto>> getAllJobs(@RequestParam Integer page, @RequestParam Integer size) {
         if(page <= 0)
             page = 1;
+
         return ResponseEntity.ok(jobService.getAll(page-1, size));
     }
 
     @PostMapping("/create")
+    @PreAuthorize("hasAuthority('MANAGER')")
     public ResponseEntity<JobDto> create (@RequestBody JobDto jobDto) {
         jobDto.setId(UUID.randomUUID());
         return ResponseEntity.ok(jobService.createEdit(jobDto));
     }
 
     @PostMapping("/edit")
+    @PreAuthorize("hasAuthority('MANAGER')")
     public ResponseEntity<JobDto> edit(
             @RequestParam(required = true) UUID id,
             @RequestBody JobDto jobDto
@@ -75,6 +55,7 @@ public class JobsController {
     }
 
     @PostMapping("/delete")
+    @PreAuthorize("hasAuthority('MANAGER')")
     public ResponseEntity<JobDto> delete(@RequestParam(required = true) UUID id) {
         jobService.removeElementById(id);
         return ResponseEntity.ok().build();
